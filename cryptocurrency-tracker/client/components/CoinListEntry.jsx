684b6f574675c8d94ecf14e146a8e6f820302c01
Faris Habib <faris.habib@protonmail.ch>
2019-07-31 15:23:44 -0700

    build: render cryptocurrency info with internal grid

--- cryptocurrency-tracker/client/components/CoinListEntry.jsx
+++ cryptocurrency-tracker/client/components/CoinListEntry.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import { Grid, Divider } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
 const CoinListEntry = (props) => {
   return (
-  <Grid.Row>
-    <Grid.Column width={2}>
-      <span>{props.asset.rank}</span>
-    </Grid.Column>
-    <Grid.Column width={3}>
-      <span>{props.asset.id}</span>
-    </Grid.Column>
-    <Grid.Column width={10}>
-      <span>{props.asset.priceUsd}</span>
-    </Grid.Column>
-    <Grid.Column width={3}>
-      <span>{props.asset.marketCapUsd}</span>
-    </Grid.Column>
-    <Grid.Column width={3}>
-      <span>{props.asset.supply}</span>
+    <Grid.Row>
+      <Grid.Column width={1}>
+        {props.asset.rank}
       </Grid.Column>
-    <Grid.Column width={3}>
-      <span>{props.asset.volumneUsd24Hr}</span>
+      <Grid.Column width={1}>
+        {props.asset.id}
       </Grid.Column>
-    <Grid.Column width={3}>
-      <span>{props.asset.changePercent24Hr}</span>
-    </Grid.Column>
-  </Grid.Row>
+      <Grid.Column width={2}>
+        {Number(props.asset.priceUsd).toFixed(2)}
+      </Grid.Column>
+      <Grid.Column width={2}>
+        {Number(props.asset.marketCapUsd).toFixed(2)}
+      </Grid.Column>
+      <Grid.Column width={2}>
+        {Number(props.asset.supply).toFixed(2)}
+        </Grid.Column>
+      <Grid.Column width={2}>
+        {Number(props.asset.volumeUsd24Hr).toFixed(2)}
+      </Grid.Column>
+      <Grid.Column width={2}>
+        {Number(props.asset.changePercent24Hr).toFixed(2)}
+      </Grid.Column>
+    </Grid.Row>
   );
 }
 
