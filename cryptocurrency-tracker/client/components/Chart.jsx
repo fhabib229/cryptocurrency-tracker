e15691a8ada4d520f1adf6bc65e1cc35b4173c52
Faris Habib <faris.habib@protonmail.ch>
2019-07-31 13:12:17 -0700

    refactor: complete chart refactor

---
+++ cryptocurrency-tracker/client/components/Chart.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import axios from 'axios';
+import moment from 'moment';
+import { Line } from 'react-chartjs-2';
+
+class Chart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      error: null,
+      data: {},
+      options: {
+        scales: {
+          xAxes: [{
+            ticks: {
+              source: 'data',
+              autoSkip: true
+            }
+          }],
+          yAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'BTC Price ($USD)'
+            }
+          }]
+        }
+      }
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.coincap.io/v2/assets/bitcoin/history?interval=d1')
+      .then(result => {
+        this.setState({
+          isLoaded: true,
+          data: result.data
+        });
+      }, (err) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  render() {
+    const { isLoaded, error, data, options } = this.state;
+    if (error) {
+      return <div>Error....{error.message}</div>
+    } else if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      let cryptoData = data.data.filter(element => element.time >= 1559347200000).sort((a, b) => a.time - b.time);
+      const btcLabels = cryptoData.map(element => moment(element.date).format('MMMM DD YYYY'));
+      const btcData = cryptoData.map(element => element.priceUsd);
+      const chartData = {
+        labels:
+          btcLabels,
+        datasets: [{
+          label: 'Bitcoin Price(BTC)',
+          backgroundColor: 'rgb(12, 115, 218)',
+          borderColor: 'rgb(58, 59, 60)',
+          data:
+            btcData
+        }]
+      };
+      return (
+        <div>
+          <Line
+            data= {chartData}
+            height={600}
+            width={1000}
+            options={options}
+          />
+        </div>
+      );
+    }
+  }
+}
+
+export default Chart;
