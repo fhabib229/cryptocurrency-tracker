f897a13abf1fe6146ec0ee2191d3bb6c70eec288
Faris Habib <faris.habib@protonmail.ch>
2019-07-31 19:47:12 -0700

    build: limit number of cryptocurrencies to render w/ option to view all

--- cryptocurrency-tracker/client/components/CoinList.jsx
+++ cryptocurrency-tracker/client/components/CoinList.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import axios from 'axios';
-import { Grid, Dimmer, Loader } from 'semantic-ui-react';
+import { Grid, Dimmer, Loader, Button } from 'semantic-ui-react';
 import CoinListEntry from './CoinListEntry.jsx';
 
 //TODO:
 //  Render chart of cryptocurrency selected by the user
+//  Styling: position button to center, color background and modify font
 
 class CoinList extends React.Component {
   constructor(props) {
@@ -12,8 +13,12 @@ class CoinList extends React.Component {
     this.state = {
       isLoaded: false,
       error: null,
-      assets: {}
+      assets: {},
+      itemsToShow: 25,
+      expanded: false
     };
+
+    this.viewMore = this.viewMore.bind(this);
   }
 
   componentDidMount() {
@@ -32,8 +37,18 @@ class CoinList extends React.Component {
       );
   }
 
+  viewMore() {
+    this.state.itemsToShow === 25 ? (
+      this.setState({
+        itemsToShow: this.state.assets.data.length,
+        expanded: true
+      })) : (
+        this.setState({ itemsToShow: 25, expanded: false })
+      )
+  }
+
   render() {
-    const { assets, isLoaded, error } = this.state;
+    const { assets, isLoaded, error, itemsToShow, expanded } = this.state;
     if (error) {
       return <div>Error....{error.message}</div>
     } else if (!isLoaded) {
@@ -44,34 +59,45 @@ class CoinList extends React.Component {
       );
     } else {
       return (
-        <Grid celled="internally">
-          <Grid.Row>
-            <Grid.Column width={1}>
-              Rank
-            </Grid.Column>
-            <Grid.Column width={5}>
-              Name
-            </Grid.Column>
-            <Grid.Column width={2}>
-              Price
-            </Grid.Column>
-            <Grid.Column width={2}>
-              Market Cap
-            </Grid.Column>
-            <Grid.Column width={2}>
-              Supply
-            </Grid.Column>
-            <Grid.Column width={2}>
-              Volume (24Hr)
-            </Grid.Column>
-            <Grid.Column width={2}>
-              Change (24Hr)
-            </Grid.Column>
-          </Grid.Row>
-          {assets.data.map(coin =>
-            <CoinListEntry key={coin.rank} asset={coin} />
-          )}
-        </Grid>
+        <div>
+          <Grid celled="internally">
+            <Grid.Row>
+              <Grid.Column width={1}>
+                Rank
+              </Grid.Column>
+              <Grid.Column width={5}>
+                Name
+              </Grid.Column>
+              <Grid.Column width={2}>
+                Price
+              </Grid.Column>
+              <Grid.Column width={2}>
+                Market Cap
+              </Grid.Column>
+              <Grid.Column width={2}>
+                Supply
+              </Grid.Column>
+              <Grid.Column width={2}>
+                Volume (24Hr)
+              </Grid.Column>
+              <Grid.Column width={2}>
+                Change (24Hr)
+              </Grid.Column>
+            </Grid.Row>
+            {assets.data.slice(0, itemsToShow).map((coin, i) =>
+              <CoinListEntry key={i} asset={coin} />
+            )}
+          </Grid>
+          <Button onClick={this.viewMore}>
+            {expanded ?
+              (
+                <span>View Less</span>
+              ) : (
+                <span>View More</span>
+              )
+            }
+          </Button>
+        </div>
       );
     }
   }
