25a03061f39cdc09c1bf271e9a06048a8304ffa7
Faris Habib <faris.habib@protonmail.ch>
2019-08-01 13:25:56 -0700

    style: capitalize app

--- cryptocurrency-tracker/client/src/index.jsx
+++ cryptocurrency-tracker/client/src/index.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import App from './app.jsx';
+import App from './App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('price-data'));
